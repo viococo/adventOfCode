b118f579caadd585f42d81172dda0d9f01606785
viococo <violaine.bodolec@pickyourskills.com>
2022-12-07 14:30:28 +0100

    update day template

--- dayTemplate/main.py
+++ dayTemplate/main.py
@@ -2,16 +2,25 @@ def exo1(arr):
   for value in arr:
     print (value)
   res = 0
-  print("exo 1", res)
+  return res
 
 def exo2(arr):
   res = 0
-  print("exo 2", res)
+  return res
 
+def solve(data):
+  print("  Exo 1:", exo1(data))
+  print("  Exo 2:", exo2(data))
 
 if __name__ == "__main__":
-  with open("data.txt","r") as txt:
-    arr = txt.readlines()
+  print("Example")
+  with open("example.txt","r") as example:
+    example_arr = example.readlines()
+    example_arr = [value.strip() for value in example_arr]
+    solve(example_arr)
 
-    exo1(arr)
-    exo2(arr)
+  print("Data")
+  with open("data.txt","r") as data:
+    data_arr = data.readlines()
+    data_arr = [value.strip() for value in data_arr]
+    solve(data_arr)
